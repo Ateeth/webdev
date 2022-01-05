063c7f8517f3dab699b9db1b6f9edb48bcbfce29
Ateeth Arun <58625369+Ateeth@users.noreply.github.com>
2022-01-05 17:09:54 +0400

    Update index.js

--- 5) Drum Kit/js/index.js
+++ 5) Drum Kit/js/index.js
@@ -4,16 +4,18 @@ for (var i = 0; i < numberOfDrumButtons; ++i) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonClickedInnerHTML = this.innerHTML;
-    makeSound(buttonClickedInnerHTML) ;
+    makeSound(buttonClickedInnerHTML);
+    buttonAnimation(buttonClickedInnerHTML);
   });
 
 }
 
-document.addEventListener("keydown" , function(event){
-  makeSound(event.key) ;
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
-function makeSound(key){
+function makeSound(key) {
   switch (key) {
     case "w":
       var audio = new Audio("sounds/tom-1.mp3");
@@ -48,4 +50,11 @@ function makeSound(key){
   }
 }
 
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
 
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
